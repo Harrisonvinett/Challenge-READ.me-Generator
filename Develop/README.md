11aa8992356b6ee524788b39e8ae86d053fe8068
harrisonvinett <harrisonvinett@gmail.com>
2022-08-11 20:42:40 -0500

    second commit

---
+++ Develop/README.md
@@ -0,0 +1,40 @@
+   # Professional README Generator
+
+        ## Description
+        A command-line application that dynamically generates a professional README.md file from a user's input using the [Inquirer package]. (THIS IS A GENERATED README!!)
+
+        ## Table of Contents
+        
+
+        ## Installation
+
+        1) Download Project  2) Run File in Terminal
+
+        ## Usage
+
+        1) Download Project  2) Run File in Terminal
+
+        ## Credits
+
+        ME, MYSELF, AND I
+
+        ## Lincense
+
+        mit
+
+        - - - - 
+
+        ## How to Contribute
+
+        MetaMask: 0x068c957d73a6aC71919718218CCF875983D2cC3b
+
+        ## Tests
+
+        
+
+        ## Questions
+
+        email: harrisonvinett@gmail.com
+    
+        🏎 - - - - - - - - - - - - - - - - - - - - - - - - - 
+     
---
+++ Develop/index.js
@@ -0,0 +1,108 @@
+
+  const inquirer = require('inquirer');
+  const fs = require('fs');
+  
+  const generateReadMe = ({ name, description, tableOfContents, installation, usage, credits, lincense, contributing, tests, questions }) =>
+    `   # ${name}
+
+        ## Description
+        ${description}
+
+        ## Table of Contents
+        ${tableOfContents}
+
+        ## Installation
+
+        ${installation}
+
+        ## Usage
+
+        ${usage}
+
+        ## Credits
+
+        ${credits}
+
+        ## Lincense
+
+        ${lincense}
+
+        - - - - 
+
+        ## How to Contribute
+
+        ${contributing}
+
+        ## Tests
+
+        ${tests}
+
+        ## Questions
+
+        ${questions}
+    
+        🏎 - - - - - - - - - - - - - - - - - - - - - - - - - 
+     `;
+  
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is your application's name?",
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter project description',
+      },
+      {
+        type: 'input',
+        name: 'tableOfContents',
+        message: 'Add tableOfContents',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use:',
+      },
+      {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators . . . ',
+      },
+      {
+        type: 'list',
+        name: 'lincense',
+        message: 'Which lincense is your project using?',
+        choices: ['apache-2.0','artistic-2.0','mit','cc','mpl-2.0','postgresql','ofl-1.1']
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'How will other users contribute',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'How to run your application:',
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: 'How a user will ask a question about application:',
+      },
+    ])
+    .then((answers) => {
+      const readMePageContent = generateReadMe(answers);
+  
+      fs.writeFile('README.md', readMePageContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created index.html!')
+      );
+    });
+  
